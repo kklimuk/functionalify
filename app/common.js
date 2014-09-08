96df4af3b42ac62751e36c97355884eb84eec927
Kirill Klimuk <kklimuk@gmail.com>
2014-09-07 21:54:50 -0700

    make the properties writable

--- app/common.js
+++ app/common.js
@@ -1,15 +1,22 @@
 module.exports = {
-    identity: function (value) { return value; },
-    noop: function () {},
+    identity: function (value) {
+        return value;
+    },
+    noop: function () {
+    },
     hasFlatMap: function (val) {
         return (typeof val === "object" || typeof val === "function") && typeof val.flatMap === "function";
     },
-    makeProperty: function (type, func) {
-        var prop = {
-            writable: true
-        };
-        prop[type] = func;
 
-        return prop;
+    makeProperty: function (type, func) {
+        if (type === 'get') {
+            return {
+                get: func
+            }
+        }
+        return {
+            writable: true,
+            value: func
+        };
     }
 };
