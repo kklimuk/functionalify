b11e984f56f1d5b18978f666d34a1259aaeb36e8
Kirill Klimuk <kklimuk@gmail.com>
2014-09-11 08:48:09 -0700

    adding bower.json

--- app/common.js
+++ app/common.js
@@ -2,8 +2,10 @@ module.exports = {
     identity: function (value) {
         return value;
     },
+
     noop: function () {
     },
+
     hasFlatMap: function (val) {
         return (typeof val === "object" || typeof val === "function") && typeof val.flatMap === "function";
     },
